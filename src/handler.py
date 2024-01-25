cf3f332a49fac5bc5120557a0d1ccab1391ae027
jmeisele <jmeisele@yahoo.com>
2024-01-25 11:34:08 +0000

    :construction:

--- src/handler.py
+++ src/handler.py
@@ -10,8 +10,14 @@ def lambda_handler(event: Dict, context: Dict) -> Dict[str, Any]:
     print(f"Event: {event}:")
     print(f"Context: {context}:")
 
-    user_response = client.list_user_profiles(DomainIdEquals=event["domain_id"])
-    print(f"user_response: {user_response}")
+    user_profile_response = client.list_user_profiles(DomainIdEquals=event["domain_id"])
+    print(f"user_profile_response: {user_profile_response}")
+
+    user_profiles = [
+        x["UserProfileName"] for x in user_profile_response["UserProfiles"]
+    ]
+    print(f"user_profiles: {user_profiles}")
+
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
